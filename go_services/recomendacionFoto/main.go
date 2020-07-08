886ad7ade0ce3230fca178dabf0f03dfd98305a1
jroma96 <romaj1805@gmail.com>
2020-07-08 14:34:33 -0700

    fix imports
    paquetes para dynamoDB

--- go_services/recomendacionFoto/main.go
+++ go_services/recomendacionFoto/main.go
@@ -4,6 +4,10 @@ import (
 	"fmt"
 	"github.com/aws/aws-lambda-go/events"
 	"github.com/aws/aws-lambda-go/lambda"
+	"github.com/aws/aws-sdk-go/aws"
+    "github.com/aws/aws-sdk-go/aws/session"
+    "github.com/aws/aws-sdk-go/service/dynamodb"
+    "github.com/aws/aws-sdk-go/service/dynamodb/dynamodbattribute"
 )
 
 func handler(request events.APIGatewayProxyRequest) (events.APIGatewayProxyResponse, error) {

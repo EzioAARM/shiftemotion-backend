ca7a3628ccc93358f7fb41234b274a812318924f
jroma96 <romaj1805@gmail.com>
2020-07-10 16:36:16 -0700

    login a spotify
    codigo base para logearse a spotify, hay que setear el redirect y agregar el uri al dashboard de devs en spotify

--- go_services/login/go.mod
+++ go_services/login/go.mod
@@ -1,4 +1,7 @@
-require github.com/aws/aws-lambda-go v1.13.3
+require (
+	github.com/aws/aws-lambda-go v1.13.3
+	github.com/zmb3/spotify v0.0.0-20200525010707-bc712583571e
+)
 
 module hello-world
 
--- go_services/login/main.go
+++ go_services/login/main.go
@@ -2,9 +2,19 @@ package main
 
 import (
 	"fmt"
-
 	"github.com/aws/aws-lambda-go/events"
 	"github.com/aws/aws-lambda-go/lambda"
+	"github.com/zmb3/spotify"
+	"log"
+	"net/http"
+)
+
+const redirectURI = "http://localhost:8080/callback"
+
+var (
+	auth  = spotify.NewAuthenticator(redirectURI, spotify.ScopeUserReadPrivate)
+	ch    = make(chan *spotify.Client)
+	state = "abc123"
 )
 
 func handler(request events.APIGatewayProxyRequest) (events.APIGatewayProxyResponse, error) {
@@ -14,6 +24,40 @@ func handler(request events.APIGatewayProxyRequest) (events.APIGatewayProxyRespo
 	}, nil
 }
 
+func completeAuth(w http.ResponseWriter, r *http.Request) {
+	tok, err := auth.Token(state, r)
+	if err != nil {
+		http.Error(w, "Couldn't get token", http.StatusForbidden)
+		log.Fatal(err)
+	}
+	if st := r.FormValue("state"); st != state {
+		http.NotFound(w, r)
+		log.Fatalf("State mismatch: %s != %s\n", st, state)
+	}
+	// use the token to get an authenticated client
+	client := auth.NewClient(tok)
+	fmt.Fprintf(w, "Login Completed!")
+	ch <- &client
+}
+
 func main() {
+	auth.SetAuthInfo("d133bb8c721e476db214f1319fce2b11", "ecd0db16e5334a1f9d34a548c1624534")
+	http.HandleFunc("/callback", completeAuth)
+	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
+		log.Println("Got request for:", r.URL.String())
+	})
+	go http.ListenAndServe(":8080", nil)
+
+	url := auth.AuthURL(state)
+	fmt.Println("Please log in to Spotify by visiting the following page in your browser:", url)
+
+	client := <-ch
+
+	user, err := client.CurrentUser()
+	if err != nil {
+		log.Fatal(err)
+	}
+	fmt.Println("You are logged in as:", user.ID)
+
 	lambda.Start(handler)
 }

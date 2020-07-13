b8db22479404466e3718dbe001fac28bc753ac59
jroma96 <romaj1805@gmail.com>
2020-07-12 19:22:05 -0700

    Update main.go

--- go_services/login_token/main.go
+++ go_services/login_token/main.go
@@ -4,15 +4,15 @@ import (
 	"fmt"
 	"github.com/aws/aws-lambda-go/events"
 	"github.com/aws/aws-lambda-go/lambda"
-	"github.com/aws/aws-sdk-go/aws/session"
-	"github.com/aws/aws-sdk-go/service/dynamodb"
-	"github.com/aws/aws-sdk-go/service/dynamodb/dynamodbattribute"
+	//"github.com/aws/aws-sdk-go/aws/session"
+	//"github.com/aws/aws-sdk-go/service/dynamodb"
+	//"github.com/aws/aws-sdk-go/service/dynamodb/dynamodbattribute"
 	"os"
-	"strconv"
+	//"strconv"
 )
 
 func handler(request events.APIGatewayProxyRequest) (events.APIGatewayProxyResponse, error) {
-	token:= request.token
+	//token := request.token
 	return events.APIGatewayProxyResponse{
 		Body:       fmt.Sprintf("Prueba desde AWS Lambda con GO"),
 		StatusCode: 200,
@@ -20,5 +20,6 @@ func handler(request events.APIGatewayProxyRequest) (events.APIGatewayProxyRespo
 }
 
 func main() {
+	fmt.Println(os.Getenv("AWS_SECRET_ACCESS_KEY"))
 	lambda.Start(handler)
 }

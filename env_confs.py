c62f52e6f413727654701cf6d88833a20386e3fb
Axel Rodriguez <alejandrom9712@gmail.com>
2020-07-15 10:26:02 -0600

    Cambio de REGION a AWS_REGION de variable de entorno

--- env_confs.py
+++ env_confs.py
@@ -5,9 +5,9 @@ with open('template.yaml', 'r') as file:
 
 print(os.environ)
 
-filedata = filedata.replace('${AWS_ACCESS_KEY_ID}', os.environ['AWS_ACCESS_KEY_ID'])
-filedata = filedata.replace('${REGION}', os.environ['REGION'])
-filedata = filedata.replace('${AWS_SECRET_ACCESS_KEY}', os.environ['AWS_SECRET_ACCESS_KEY'])
+filedata = filedata.replace('AWS_ACCESS_KEY_ID', os.environ['AWS_ACCESS_KEY_ID'])
+filedata = filedata.replace('REGION', os.environ['REGION'])
+filedata = filedata.replace('AWS_SECRET_ACCESS_KEY', os.environ['AWS_SECRET_ACCESS_KEY'])
 
 with open('file.txt', 'w') as file:
     file.write(filedata)

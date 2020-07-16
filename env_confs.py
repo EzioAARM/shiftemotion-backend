89b62935ecdbd7cde9e86c4c075a0f235ca6f6dd
Axel Rodriguez <alejandrom9712@gmail.com>
2020-07-15 19:30:06 -0600

    Corrección de referencias de variables y cambio de region en el script de python

--- env_confs.py
+++ env_confs.py
@@ -3,10 +3,8 @@ import os
 with open('template.yaml', 'r') as file:
     filedata = file.read()
 
-print(os.environ)
-
 filedata = filedata.replace('AWS_ACCESS_KEY_ID', os.environ['AWS_ACCESS_KEY_ID'])
-filedata = filedata.replace('REGION', os.environ['AWS_REGION'])
+filedata = filedata.replace('REGION', os.environ['AWS_DEFAULT_REGION'])
 filedata = filedata.replace('AWS_SECRET_ACCESS_KEY', os.environ['AWS_SECRET_ACCESS_KEY'])
 
 with open('file.txt', 'w') as file:
